92fcf53b97415bf58ae7a0af0395db3749a5811b
Tshewang Gyaltshen <tshewanggyaltshen0@gmail.com>
2024-12-06 19:54:55 +0600

    the man who controls the emotions controls the world

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,7 +2,44 @@ import React from 'react'
 import './detail.css';
 function Detail() {
     return (
-        <div className='detail'>Detail</div>
+        <div className='detail'>
+            <div className="user">
+                <img src="./avatar.png" alt="" />
+                <h2>Db copper</h2>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="info">
+                <div className="option">
+                    <div className="title">
+                        <span>Shared photo</span>
+                        <img src="./arrowDown.png" alt="" />
+                    </div>
+                    <div className="photos">
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="" alt="" />
+                                <span>photo_2024.png</span>
+                            </div>
+                            <img src="./donwload.png" alt="" className='icon' />
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="" alt="" />
+                                <span>photo_2024.png</span>
+                            </div>
+                            <img src="./donwload.png" alt="" className='icon' />
+                        </div>
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Shared Files</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+            </div>
+            <button>Block User</button>
+        </div>
     )
 }
 
